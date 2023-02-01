2c59cd36bd96383e772fe1d0c177ddc5eba8e7a2
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2023-02-01 16:35:06 -0300

    aula 2 finalizada

--- project/backend/api/views.py
+++ project/backend/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .seriealizers import RoomSerializer
+from .models import Room
 
-# Create your views here.
+#class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
+  queryset = Room.objects.all()
+  serializer_class = RoomSerializer
+
